896c81832e0feafab5a70877e6a24dc8e4ee8a1b
Lucas Vinicius <lucasidelfonso73@gmail.com>
2025-02-13 15:53:05 -0300

    Fix: Adding listenable in DropDownPregnant

--- lib/ui/pregnant/widgets/pregnant_criteria_dropdown.dart
+++ lib/ui/pregnant/widgets/pregnant_criteria_dropdown.dart
@@ -8,40 +8,43 @@ class PregnantCriteriaDropdown extends StatelessWidget {
   Widget build(BuildContext context) {
     final PregnantViewModel viewModel = PregnantViewModel();
 
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      decoration: BoxDecoration(
-          border: Border.all(
-            color: Color(0xFFE7E7E7),
+    return ListenableBuilder(
+      listenable: viewModel,
+      builder: (context, child) => Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+            border: Border.all(
+              color: Color(0xFFE7E7E7),
+            ),
+            borderRadius: BorderRadius.circular(6)),
+        width: double.infinity,
+        height: 41,
+        child: DropdownButtonHideUnderline(
+            child: DropdownButton(
+          value: viewModel.criteria,
+          isExpanded: true,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            height: 0,
+            overflow: TextOverflow.clip,
           ),
-          borderRadius: BorderRadius.circular(6)),
-      width: double.infinity,
-      height: 41,
-      child: DropdownButtonHideUnderline(
-          child: DropdownButton(
-        value: viewModel.criteria,
-        isExpanded: true,
-        style: const TextStyle(
-          color: Colors.black,
-          fontSize: 20,
-          fontWeight: FontWeight.w400,
-          height: 0,
-          overflow: TextOverflow.clip,
-        ),
-        items: viewModel.criteriaList.map<DropdownMenuItem<String>>((value) {
-          return DropdownMenuItem<String>(
-              value: value,
-              child: Text(
-                value,
-                style: TextStyle(
-                  color: Colors.black,
-                ),
-              ));
-        }).toList(),
-        onChanged: (String? value) {
-          viewModel.changeDropDown(value);
-        },
-      )),
+          items: viewModel.criteriaList.map<DropdownMenuItem<String>>((value) {
+            return DropdownMenuItem<String>(
+                value: value,
+                child: Text(
+                  value,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ));
+          }).toList(),
+          onChanged: (String? value) {
+            viewModel.changeDropDown(value);
+          },
+        )),
+      ),
     );
   }
 }
